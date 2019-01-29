67dad9730272e1705a9f0f9c59699f3c65a09e3d
LuanComputacao <eng.luanroger@gmail.com>
2019-01-29 02:02:03 -0200

    more views

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,7 +2,7 @@ $(function () {
     $('#anjelim-box').entChat({
         selfServURL: 'http://localhost:8082/support',
         chatURL: 'http://localhost:8082/chat',
-        socketURL: '/message',
+        socketURL: 'http://localhost:8082/message',
         clientInfo: {
             token: 'a1a2a3'
         }
